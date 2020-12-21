f33c0877a3b6e4d08126cfb9b6662ed3295cd772
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 03:59:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,13 +17,13 @@ def output_array (message)
   end
 end
 
-def return_string_array (array)
+def return_string_array (message)
     i=0
-  while i < array.length do
-    array[i]=array(i).to_s
+  while i < message.length do
+    message[i]=message(i).to_s
     i=i+1
   end
-  puts array
+  puts message
 end
 
 array = [5, 4, 3, 2, 1]
