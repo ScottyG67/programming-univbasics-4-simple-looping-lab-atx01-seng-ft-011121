2a36ba8924c6b86ad067e803da70f12851495400
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 03:47:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,12 @@
 # Write your methods here
 
 def loop_message_five_times (message)
-  i=0
-  while i<5 do
-    puts message
-    i=i+1
-  end
+  5.times puts message
+  #i=0
+  #while i<5 do
+  #  puts message
+  #  i=i+1
+  #end
 end
 
 def loop_message_n_times (message,number)
