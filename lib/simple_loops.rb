4216f9ca933ccacea46fe1d1f0f9644390bef734
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 03:41:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,10 @@
 # Write your methods here
 
-def loop_message_five_times
+def loop_message_five_times (message)
   
 end
 
-def loop_message_n_times
+def loop_message_n_times (message,number)
   
 end 
 
